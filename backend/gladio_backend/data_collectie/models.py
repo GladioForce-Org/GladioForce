07801d7c84c6da9fb6f54e9871c800b8517ffffb
r0831281 - Jonas <r0831281@student.thomasmore.be>
2024-12-02 18:16:07 +0100

    database test data werkt

--- backend/gladio_backend/data_collectie/models.py
+++ backend/gladio_backend/data_collectie/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-import hashlib
-
 
 class Club(models.Model):
     name = models.CharField(max_length=100)
@@ -10,19 +8,21 @@ class Club(models.Model):
     link = models.URLField()
     bank_account = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    BTW_number = models.CharField(max_length=100)
+    btw_number = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
+
 class Edition(models.Model):
     year = models.IntegerField()
 
     def __str__(self):
         return str(self.year)
-    
+
+
 class Size(models.Model):
     size = models.CharField(max_length=100)
 
@@ -36,59 +36,63 @@ class Tshirt(models.Model):
 
     def __str__(self):
         return self.model
-    
-class availableTshirt(models.Model):
+
+
+class AvailableTshirt(models.Model):
     tshirt = models.ForeignKey(Tshirt, on_delete=models.CASCADE)
     edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return str(self.tshirt) + ' ' + str(self.edition)
-    
+        return f"{self.tshirt} - {self.edition}"
+
+
 class Volunteer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     national_registry_number = models.CharField(max_length=100)
-    works_day1 = models.BooleanField()
-    works_day2 = models.BooleanField()
-    needs_parking_day1 = models.BooleanField()
-    needs_parking_day2 = models.BooleanField()
-    tshirt = models.ForeignKey(availableTshirt, on_delete=models.CASCADE)
+    works_day1 = models.BooleanField(default=False)
+    works_day2 = models.BooleanField(default=False)
+    needs_parking_day1 = models.BooleanField(default=False)
+    needs_parking_day2 = models.BooleanField(default=False)
+    tshirt = models.ForeignKey(AvailableTshirt, on_delete=models.CASCADE)
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
 
-
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
-    
-class participating_club(models.Model):
+        return f"{self.first_name} {self.last_name}"
+
+
+class ParticipatingClub(models.Model):
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
     person_in_charge_day1 = models.CharField(max_length=100)
     person_in_charge_day2 = models.CharField(max_length=100)
 
     def __str__(self):
-        return str(self.club) + ' ' + str(self.edition)
-    
+        return f"{self.club} - {self.edition}"
+
+
 class Task(models.Model):
     description = models.TextField()
-    day = models.IntegerField()
+    day = models.IntegerField(choices=[(1, "Day 1"), (2, "Day 2")])
     volunteers_required = models.IntegerField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     responsible_employee = models.CharField(max_length=100)
-    participating_club = models.ForeignKey(participating_club, on_delete=models.CASCADE)
+    participating_club = models.ForeignKey(ParticipatingClub, on_delete=models.CASCADE)
     edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.description + ' ' + str(self.day) + ' ' + str(self.edition)
-    
+        return f"{self.description} - Day {self.day} - {self.edition}"
+
+
 class TimeRegistration(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
-    day = models.IntegerField()
+    day = models.IntegerField(choices=[(1, "Day 1"), (2, "Day 2")])
     edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
     volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.volunteer) + ' ' + str(self.day) + ' ' + str(self.edition)
+        return f"{self.volunteer} - Day {self.day} - {self.edition}"
