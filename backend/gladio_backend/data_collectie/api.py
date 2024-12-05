61d64effcca33e70ccc32139cf8e430eee6847d3
r0831281 - Jonas <r0831281@student.thomasmore.be>
2024-12-05 18:11:49 +0100

    post club

--- backend/gladio_backend/data_collectie/api.py
+++ backend/gladio_backend/data_collectie/api.py
@@ -1,5 +1,6 @@
 from ninja import NinjaAPI
-from .models import Club
+from .models import Club, ParticipatingClub
+from .schemas import ClubCreateSchema
 
 api = NinjaAPI()
 
@@ -11,3 +12,25 @@ def hello_world(request):
 def get_clubs(request):
     clubs = Club.objects.all()
     return list(clubs.values())
+
+#create club
+@api.post("/clubs")
+def create_club(request, payload: ClubCreateSchema):
+    club = Club.objects.create(**payload.dict())
+    return {"id": club.id, "name": club.name}
+
+
+@api.get("/participating_clubs")
+def get_participating_clubs(request):
+    participating_clubs = (
+        ParticipatingClub.objects.select_related("club")
+        .values(
+            "id",
+            "club_id",
+            "club__name",  # Fetch the club name directly
+            "edition_id",
+            "person_in_charge_day1",
+            "person_in_charge_day2",
+        )
+    )
+    return list(participating_clubs)
