528955f6ea4ef3ac6a2c2ad11deef16aba983838
Dries <driesvandoorne@hotmail.com>
2024-12-06 03:47:27 +0100

    feather icons imported

--- frontend/admin_dashboard/src/app/sidebar/link/link.component.ts
+++ frontend/admin_dashboard/src/app/sidebar/link/link.component.ts
@@ -1,21 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeatherIconComponent } from '../../feather-icon/feather-icon.component';
 
 @Component({
   selector: 'app-link',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FeatherIconComponent],
   templateUrl: './link.component.html',
   styleUrl: './link.component.scss',
 })
 export class LinkComponent implements OnInit {
   @Input() linkName: string = '';
-  @Input() link: string = ''
-  @Input() hasIcon: boolean = false
+  @Input() link: string = '';
+  @Input() icon: string = '';
+
   css: String = 'flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-purple-400 dark:text-white dark:hover:bg-gray-700 group'
 
   async ngOnInit() {
-    if (this.hasIcon) {
+    if (this.icon !== '') {
       //this.css = 'flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-purple-400 dark:hover:bg-gray-700 group'
       this.css = "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-purple-400 dark:hover:bg-gray-700 dark:text-white group"
     }
