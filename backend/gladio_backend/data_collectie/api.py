96b81afdcf7749fd54e3c9c1ccc587bfd9abab79
r0831281 - Jonas <r0831281@student.thomasmore.be>
2024-12-10 19:37:34 +0100

    tags

--- backend/gladio_backend/data_collectie/api.py
+++ backend/gladio_backend/data_collectie/api.py
@@ -1,9 +1,8 @@
 from ninja import NinjaAPI
-from .clubApi import router as club_router
-from .volunteerApi import router as volunteer_router
+from . import clubApi, volunteerApi
+
 
 api = NinjaAPI()
 
-# Add the routers with different prefixes
-api.add_router("/clubs", club_router)
-api.add_router("/volunteers", volunteer_router)
+api.add_router("/clubs", clubApi.router)
+api.add_router("/volunteers", volunteerApi.router)
