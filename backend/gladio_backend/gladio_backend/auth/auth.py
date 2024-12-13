6ba611f00c462f00357beccf04dc43a1b243678a
r0831281 - Jonas <r0831281@student.thomasmore.be>
2024-12-13 11:32:41 +0100

    get token from headers

--- backend/gladio_backend/gladio_backend/auth/auth.py
+++ backend/gladio_backend/gladio_backend/auth/auth.py
@@ -2,8 +2,11 @@ from ninja.security import HttpBearer
 from firebase_admin import auth
 
 class FirebaseAuth(HttpBearer):
-    def authenticate(self, request, token):
+    def authenticate(self, request):
         try:
+            print(request.headers)
+            #get token form request header
+            token = request.headers.get('Authorization').split('Bearer ')[1]
             # Verify the Firebase ID token
             decoded_token = auth.verify_id_token(token)
             return decoded_token  # Return the decoded token for use in the endpoint
