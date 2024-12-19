0e214af1f4dd5605f6f680256caa6c07d8de4591
r0831281 - Jonas <r0831281@student.thomasmore.be>
2024-12-19 20:45:46 +0100

    double api instance

--- backend/gladio_backend/data_collectie/admin_api/clubApi.py
+++ backend/gladio_backend/data_collectie/admin_api/clubApi.py
@@ -30,38 +30,3 @@ def delete_club(request, club_id: int):
     Club.objects.get(id=club_id).delete()
     return {"status": "ok"}
 
-
-
-#moved: not authenticate with JWT
-# @router.get("/{club_link}", response=ClubSchemaOut)
-# def get_club(request, club_link: str):
-#     club = Club.objects.get(link = club_link)
-#     return club
-
-#get all volunteers of a club
-# @router.get("/{club_link}/volunteers", response=List[VolunteerSchemaOut])
-# def get_volunteers(request, club_link: str):
-#     club = Club.objects.get(link = club_link)
-#     volunteers = Volunteer.objects.filter(club = club)
-#     return list(volunteers)
-
-
-
-#moved: not authenticate with JWT
-# @router.patch("update/{club_link}")
-# def update_club(request, club_link: str, payload: ClubSchemaPatch):
-#     club = Club.objects.get(link=club_link)
-#     club.name = payload.name
-#     club.email = payload.email
-#     club.contact = payload.contact
-#     club.phone = payload.phone
-#     club.bank_account = payload.bank_account
-#     club.address = payload.address
-#     club.btw_number = payload.btw_number
-#     club.postal_code = payload.postal_code
-#     club.city = payload.city
-#     club.save()
-#     return {"status": "ok"}
-
-
-api = NinjaAPI()
