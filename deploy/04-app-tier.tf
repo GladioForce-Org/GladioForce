84942278561453914f82e126c4550a98c8b826c8
Pieter Moons <r0785469@student.thomasmore.be>
2025-01-07 10:24:04 +0100

    update deploy.yaml; production-test

--- deploy/04-app-tier.tf
+++ deploy/04-app-tier.tf
@@ -158,7 +158,10 @@ resource "aws_ecs_cluster_capacity_providers" "ecs_web_tier_fargate" {
 # Task definition that will deploy the container.
 # The container definitions json encoded file will configure the environment variables needed for the database connection to the RDS database
 # We will be using the secrets module of AWS, otherwise the secrets are stored in clear.
-
+resource "random_id" "ecs_task_definition_trigger" {
+  byte_length = 8
+  id          = timestamp() # Always use a different timestamp on each apply
+}
 
 
 resource "aws_ecs_task_definition" "gladioforce_backend" {
@@ -236,7 +239,7 @@ resource "aws_ecs_task_definition" "gladioforce_backend" {
 
   lifecycle {
     # Trigger replacement on each apply using the current timestamp ensuring the latest image is used
-    replace_triggered_by = [timestamp()]
+    replace_triggered_by = [random_id.ecs_task_definition_trigger.hex]
 
   }
 
@@ -265,7 +268,7 @@ resource "aws_ecs_service" "gladioforce_backend" {
   #### Service registry not used in lab ######## Service registry not used in lab ######## Service registry not used in lab ######## Service registry not used in lab ######## Service registry not used in lab ####
   lifecycle {
 
-    replace_triggered_by = [timestamp()]
+    replace_triggered_by = [random_id.ecs_task_definition_trigger.hex]
 
   }
 
